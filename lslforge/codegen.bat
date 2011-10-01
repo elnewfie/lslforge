08ef8ed2531ef5626732d5cb7ef079da17e8a8d2
Newfie Pendragon <elnewfie@gmail.com>
2011-09-30 22:56:33 -0400

    - General rebranding from LSLPlus to LslForge - Added ability to detect and auto-convert project nature to LSLForge

--- lslforge/codegen.bat
+++ lslforge/codegen.bat
@@ -1,5 +1,7 @@
 @echo off
 
+
 del eclipse\lslforge\src\lslforge\generated\*.java
+
 
 haskell\dist\build\LslForge\LslForge.exe _CodeGen_ eclipse\lslforge\src\lslforge\generated lslforge.generated
---
+++ lslforge/eclipse/lslforge/src/lslplus/LslProjectNature.java
@@ -0,0 +1,7 @@
+package lslplus;
+
+public class LslProjectNature extends lslforge.LslProjectNature {
+	@SuppressWarnings("hiding")
+	public static String ID = "lslplus.lslPlusNature";	//$NON-NLS-1$
+
+}
