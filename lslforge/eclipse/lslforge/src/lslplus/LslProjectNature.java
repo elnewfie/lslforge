4d424d1172be06d7a01880dbc1fdd43b8f3a2bf6
Richard <richard@lslplus-linux.(none)>
2012-06-16 20:45:15 -0400

    Attempt to fix case sensitivity issues in git repos.  All java files starting with 'Lsl' should now be correctly named 'LSL'.

--- lslforge/eclipse/lslforge/src/lslplus/LslProjectNature.java
+++
@@ -1,7 +0,0 @@
-package lslplus;
-
-public class LSLProjectNature extends lslforge.LSLProjectNature {
-	@SuppressWarnings("hiding")
-	public static String ID = "lslplus.lslPlusNature";	//$NON-NLS-1$
-
-}
