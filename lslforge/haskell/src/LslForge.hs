70f64f01f87bd18b52e36e3b4a1cd3beb15f7071
Ray Silent <ray.silent@gmail.com>
2016-02-19 20:13:26 -0800

    LslForge.hs, LslForge.cabal version number update

--- lslforge/haskell/src/LslForge.hs
+++ lslforge/haskell/src/LslForge.hs
@@ -13,7 +13,7 @@ import IO
 import System
 import System.Exit
 
-version="0.1.3"
+version="0.1.7"
 
 usage progName = "Usage: " ++ progName ++ " [Version|MetaData|Compiler|ExpressionHandler|SimMetaData|SystemTester|UnitTester|CompilationServer]"
 
